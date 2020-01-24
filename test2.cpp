5d7b7868992ba30058af8c3bf2aab92dc7918b9d
Ranindu15 <ranindujayasooriya@gmail.com>
2020-01-23 23:20:19 -0800

    Update case 5

--- test2.cpp
+++ test2.cpp
@@ -31,12 +31,10 @@ class Flight{
 		}
 		
 		void set_seats(string array_f[], int count){
-			int j;
-			string single_line[3];
-			int row_number;
+			int j,row_number;
+			string single_line[3]; 
 			
 			for(int i=4; i<count; i++){
-
 				istringstream ss(array_f[i]);
 				j = 0;
                 do { 
@@ -87,12 +85,11 @@ class Flight{
                   if (single_line[2].find("F") != string::npos) {
  					this->bclass[row_number][6] = true ;
                   }
-				}
-   			    
+				} 
 			}
-			
 		}
-		
+
+//to set all index as false in 2d array		
 		Flight(){
 			for(int i=0;i<61;i++){
 				for(int j=0;j<7;j++){
@@ -100,28 +97,27 @@ class Flight{
 					this->eclass[i][j]= false;
 				}
 			}
-		}
-		
+		}	
 };
 
 
 
 main(){
 
-bool temp = false;
-char seat_col,flight_class;
-int  seat_row,b_seat_count=0,e_seat_count=0,i=0,v,j,k;
-string array_file[1000];
-string temp_array_file[65];
-ifstream file("Flights.txt");
+bool temp = false,at_least_single_sheet, print_line;
+char seat_col, flight_class, col, clz;
+int  choice, k, colm, v, j, num_of_seats, row, uniq_flight, seat_row, b_seat_count=0, e_seat_count=0, i=0, flightCount=0, position =0;;
+string flight_name, array_file[1000], temp_array_file[65];
+fstream file, fileo;
+file.open("Flights.txt",ios::in);
 
-int flightCount = 0;
 
+//to read the whole file and get count of lines
 while(std::getline(file, array_file[i])) {
 	i++;
 }
 
-
+//to find the count of flight names
 for(j=0;j<i;j++){
   getline(file, array_file[i]);
   if(array_file[j] == ""){
@@ -131,16 +127,15 @@ for(j=0;j<i;j++){
 	}
   }
 }
-
 if(array_file[i-1] == ""){
 } else {
 	flightCount++;
 }
 
+//create an array of object
 Flight A[flightCount];
 
-int position = 0;
-
+//to select the details of one flight
 for(j=0;j<flightCount;j++){
 	k = 0;
     while(array_file[position] != "" ){
@@ -158,29 +153,24 @@ for(j=0;j<flightCount;j++){
 
 while (1)
 {
-	int choice,k, v=0;
-	cout<< "///////////////////////////" << endl;
+	cout<< "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~" << endl<<endl;
 	cout<< "---Flight Booking System---" << endl;
-	cout<< "///////////////////////////" << endl;
+	cout<< "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~" << endl<<endl;;
 	cout<<"1). Display Available Flights"<< endl;
 	cout<<"2). View Flight" <<endl;
 	cout<< "3). Seat Availability " <<endl;
 	cout<<"4). Seat Booking" << endl;
-	cout<<"5). Exit"<< endl;
+	cout<<"5). Exit"<< endl<<endl;;
 	cout<< "Select Your Option :- ";
 	cin >> choice;
-	cout<< "///////////////////////////" << endl;
-
-int colm;
-	string flight_name;
-	bool at_least_single_sheet;
-	int num_of_seats, row, i, uniq_flight;
-	char col, clz;
-
+	cout<< "~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~" << endl<<endl;
+ 
+	
 
 	switch (choice)
 	{
 		case 1:
+			
 			for(int k=0;k<flightCount;k++){
 				at_least_single_sheet = false;
 				for(int i=0;i<61;i++){
@@ -195,33 +185,32 @@ int colm;
 					}
 				}
 				if(at_least_single_sheet){
-					cout<< "Flight Name : " <<A[k].flight_name<<endl;
+					cout<<"Flight Name : " <<A[k].flight_name<<endl;
 					cout<<"Date : " << A[k].flight_date<<endl;
 					cout<<"Time : " << A[k].flight_time<<endl;
-					cout<< "Arrival city : "<<A[k].arriv_city<<endl;
-					cout<< "Departure city : "<< A[k].depart_city <<endl;
-					cout<< "............" <<endl;
+					cout<<"Departure city : "<< A[k].depart_city <<endl;
+					cout<<"Arrival city : "<<A[k].arriv_city<<endl;
+					cout<<"............" <<endl<<endl;
 				}
 			}
 		break;
 
 		case 2:
-			cout<< "Enter flight Name :";
+			cout<< "Enter flight Name : ";
 			cin >> flight_name;
 			temp = false;
 			for(int i=0; i< flightCount; i++){
 				if(flight_name == A[i].flight_name){
 					temp = true;
 					v=i;
-					break;
-					
+					break;	
 				}
 			}
 			if(!temp){
-				cout<<"Invalid Flight Number"<<endl<<endl;
+				cout<<"Sorry!! Invalid Flight Number"<<endl<<endl;
 				break;
 			}
-			cout<<"Business Class :-"<<endl;
+			cout<<endl<<"Business Class : "<<endl<<endl;
 			for(int i=1; i<61; i++){
 				temp = false;
 				for(int j=1; j<7; j++){
@@ -237,17 +226,12 @@ int colm;
 					}
 				}
 				if(temp){
-					cout<<"   seats available in   "<< i << "  th Row" <<endl; 
+					cout<<"   seats are available in   "<< i << "  th Row" <<endl; 
 				}		
 			}
-			cout <<"Toatal number of B class seats :: "<< b_seat_count<< endl<<endl<<endl;
+			cout<<endl<<"Toatal number of B class seats:  "<< b_seat_count<< endl<<endl<<endl;
 
-
-
-
-			cout<<"Economic Class :-"<<endl;
-			cout<<"Seats  ||   Row "<<endl;
-			e_seat_count=0;
+			cout<<endl<<"Economic Class : "<<endl<<endl;
 			for(int i=1; i<61; i++){
 				temp = false;
 				for(int j=1; j<7; j++){
@@ -265,13 +249,12 @@ int colm;
 				if(temp){
 					cout<<" seats available in    "<< i << "    th Row" <<endl; 
 				}
-				
 			}
-			cout<<"Total Number of E class seats :: "<< e_seat_count<< endl<<endl<<endl;
+			cout<<endl<<"Total Number of E class seats:  "<< e_seat_count<< endl<<endl<<endl;
 			break;
 
 			case 3:
-				cout<< "Flight Number :"<<endl;
+				cout<< "Flight Number : ";
 				cin>> flight_name;
 				temp = false;
 				for(int i=0; i< flightCount; i++){
@@ -279,15 +262,14 @@ int colm;
 						temp = true;
 						v=i;
 						break;
-						
 					}
 				}
 				if(!temp){
-					cout<<"Invalid Flight Number"<<endl<<endl;
+					cout<<"Sorry!! Invalid Flight Number."<<endl<<endl;
 					break;
 				}
-				cout<< "How many seats :"<<endl;
-				cin>> num_of_seats;
+				cout<<"How many seats would you buy :";
+				cin>>num_of_seats;
 				e_seat_count =0;
 				for(int i=1; i<61; i++){
 					for(int j=1; j<7; j++){
@@ -307,121 +289,143 @@ int colm;
 
 				if (e_seat_count >= num_of_seats || b_seat_count >= num_of_seats || (e_seat_count+b_seat_count) >= num_of_seats )
 				{
-					cout<< "seats are available" <<endl;
-					cout<< "E seats :" << e_seat_count << endl<<"B seats : "<<b_seat_count<<endl<<endl;
+					cout<< "seats are available. You can Buy!!" <<endl<<endl;
+					cout<< "Total Available 'E Class' seats :" << e_seat_count << endl<<"Total Available 'B Class' seats :"<<b_seat_count<<endl<<endl;
 				}
 				else{
-					cout<< "Not available" <<endl<<endl<<endl;
+					cout<< "Sorry Not Enough seats are available" <<endl<<endl<<endl;
 				}
 
 				break;
 			
 			case 4:
-				cout<<"Enter Flight Number : "<<endl;
-				cin>> flight_name;
+				cout<<"Enter Flight Number :";
+				cin>>flight_name;
 				temp = false;
 				for(int i=0; i< flightCount; i++){
 					if(flight_name == A[i].flight_name){
 						temp = true;
 						v=i;
 						break;
-						
 					}
 				}
 				if(!temp){
-					cout<<"Invalid Flight Number"<<endl<<endl;
+					cout<<"Sorry Invalid Flight Number"<<endl<<endl;
 					break;
 				}else{
 					cout<<"Enter your class :";
-					cin>> flight_class;
+					cin>>flight_class;
 					if(flight_class != 'E' && flight_class != 'B'){
 						cout<< "Invalid class"<<endl;
 						break;
 					}else{	
-						cout<<"Enter Row number : ";
+						cout<<"Enter Row number :";
 						cin>>seat_row;
 						if(seat_row >= 61 || seat_row <= 0){
-							cout<<"Invalid Input!"<<endl;
+							cout<<"Sorry Invalid row number!"<<endl;
 							break;
 						}else{
-							cout<<"Enter column Alphabet";
+							cout<<"Enter column Alphabet :";
 							cin>> seat_col;
 							if(seat_col !='A' && seat_col !='B' && seat_col !='C' && seat_col !='D' && seat_col !='E' && seat_col !='F'){
-								cout<<"Invalid input"<<endl;
+								cout<<"Sorry Invalid column Number"<<endl;
 								break;
 							}
-							int colm = (int)'A';
-							cout<<"A :"<< col<<endl;
-//							else if{ int col = (int)A;}
-//							else if{ int col = (int)B;}
-//							else if{ int col = (int)C;}
-//							else if{ int col = (int)D;}
-//							else if{ int col = (int)E;}
-//							else if{ int col = (int)F;}
-//			
-//								cout<<"v :"<<v<<endl<<"row: "<<seat_row<<endl<<"col :"<<seat_col<<endl;
-//								cout<<"lol :"<<A[v].eclass[seat_row+1][col]<<endl;
-								
-//								for(int i=1;i<61;i++) {
-//	 								cout<< i << " - " ;
-//								 	for(int j=1;j<7;j++) {
-//								 		cout << A[v].eclass[i][j] << " " ;
-//							         }
-//								 	cout << endl;
-//							     }
-								
-								
-//								if(flight_class == 'E'){
-//									if(A[v].eclass[seat_row][seat_col]){
-//										cout<<"Seat is Allocated!!"<<endl;
-//									}
-//									else{
-//										cout<<"Seat is not available!! Try another one"<<endl;
-//									}	
-//								}
-//				
-//								if(flight_class == 'B'){
-//									if(A[v].bclass[seat_row][seat_col]){
-//										cout<<"Seat is Allocated!!"<<endl;
-//									}
-//									else{
-//										cout<<"Seat is not available!! Try another one"<<endl;
-//									}	
-//								}
+							if(seat_col == 'A'){colm = (int)'A' -64 ;}
+							if(seat_col == 'B'){colm = (int)'B' -64 ;}
+							if(seat_col == 'C'){colm = (int)'C' -64 ;}
+							if(seat_col == 'D'){colm = (int)'D' -64 ;}
+							if(seat_col == 'E'){colm = (int)'E' -64 ;}
+							if(seat_col == 'F'){colm = (int)'F' -64 ;}
+							
+							if(flight_class == 'E'){
+								if(A[v].eclass[seat_row][colm]){
+									cout<<"Seat is Allocated!!"<<endl;
+									A[v].eclass[seat_row][colm] = false;
+									file.close();
+									file.open ("Flights.txt", ios::out );
+								}
+								else{
+									cout<<"Sorry Seat is not available!! Try another one"<<endl;
+								}	
+							}
+							if(flight_class == 'B'){
+								if(A[v].bclass[seat_row][colm]){
+									cout<<"Seat is Allocated!!"<<endl;
+									A[v].bclass[seat_row][colm] = false;
+								}
+								else{
+									cout<<"Sorry Seat is not available!! Try another one"<<endl;
+								}	
 							}
 						}
-					}	
-			//	}
-
-
+					}
+				}
+				
 				break;
+				
+			case 5:
+				fileo.open("Flights.txt", ios::out );
+				for(int i=0; i<flightCount; i++){
+					fileo<<A[i].flight_name<<endl;
+					fileo<<A[i].flight_date<<" "<<A[i].flight_time<<endl;
+					fileo<<A[i].depart_city<<endl;
+					fileo<<A[i].arriv_city<<endl;
+					
+					
+					for(int k=1; k<61; k++){
+					temp = false;
+					for(int j=1; j<7; j++){
+						if(A[i].bclass[k][j]){
+							temp = true ;
+							if( j == 1 ){ fileo<<"A"; }
+							if( j == 2 ){ fileo<<"B"; }
+							if( j == 3 ){ fileo<<"C"; }
+							if( j == 4 ){ fileo<<"D"; }
+							if( j == 5 ){ fileo<<"E"; }
+							if( j == 6 ){ fileo<<"F"; }
+						//	e_seat_count++;
+						}
+					}
+					if(temp){
+						fileo<<" "<<"B"<<" "<<k<<endl; 
+					}
+				}
 
+					for(int k=1; k<61; k++){
+					temp = false;
+					for(int j=1; j<7; j++){
+						if(A[i].eclass[k][j]){
+							temp = true ;
+							if( j == 1 ){ fileo<<"A"; }
+							if( j == 2 ){ fileo<<"B"; }
+							if( j == 3 ){ fileo<<"C"; }
+							if( j == 4 ){ fileo<<"D"; }
+							if( j == 5 ){ fileo<<"E"; }
+							if( j == 6 ){ fileo<<"F"; }
+						//	e_seat_count++;
+						}
+					}
+					if(temp){
+						fileo<<" "<<"E"<<" "<<k<<endl; 
+					}
+				}
+				
+				
+				
+				
+							
+			}
+				fileo.close();
+				cout<<"Exit!!"<<endl<<endl;
+				exit(1);
 
-
-		default:
-			cout<<"shit" <<endl;
-			break;			
-
-
+			default:
+				cout<<"Invalid Choice" <<endl<<endl;
+				break;
 	}
 
-
 }
-
-
-
-////////////////////////////////////////////////////////////////////////
-
-
-
-     for(int i=1;i<61;i++) {
-	 	cout<< i << " - " ;
-	 	for(int j=1;j<7;j++) {
-	 		cout << A[0].eclass[i][j] << " " ;
-         }
-	 	cout << endl;
-     }
-
 
 return 0;
 }
